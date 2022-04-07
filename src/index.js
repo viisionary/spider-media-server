d56ffe008d8059a1a3a4546f75d264a42d4bdf01
Mia <min_0610@icloud.com>
2022-04-07 16:51:36 +0800

    lock

--- src/index.js
+++ src/index.js
@@ -11,6 +11,35 @@ const config = {
   http: {
     port: 8000,
     allow_origin: "*"
+  },
+  trans: {
+    ffmpeg: '/usr/local/bin/ffmpeg',
+    tasks: [
+      {
+        app: 'live',
+        hls: true,
+        hlsFlags: '[hls_time=2:hls_list_size=3:hls_flags=delete_segments]',
+        dash: true,
+        dashFlags: '[f=dash:window_size=3:extra_window_size=5]'
+      }
+    ]
+  },
+  relay: {
+    ffmpeg: '/usr/local/bin/ffmpeg',
+    tasks: [
+      {
+        app: 'cctv',
+        mode: 'static',
+        edge: 'rtsp://admin:admin888@192.168.0.149:554/ISAPI/streaming/channels/101',
+        name: '0_149_101',
+        rtsp_transport : 'tcp' //['udp', 'tcp', 'udp_multicast', 'http']
+      },{
+        app: 'mv',
+        mode: 'static',
+        edge: '/Users/visionary/Downloads/videoplayback.mp4',
+        name: 'dq'
+      }
+    ]
   }
 };
 
