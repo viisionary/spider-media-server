f2ba5e60aaadcd9830b96f03cd309c8ec234e745
Mia <min_0610@icloud.com>
2022-04-07 16:58:34 +0800

    port

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const NodeMediaServer = require("node-media-server");
 
 const config = {
   rtmp: {
-    port: 1935,
+    port: process.env.PORT || 1935,
     chunk_size: 60000,
     gop_cache: true,
     ping: 30,
