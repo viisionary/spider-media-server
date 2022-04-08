0c53df40502b78bf2c18532b7246e785e429000e
Mia <min_0610@icloud.com>
2022-04-08 16:11:22 +0800

    modify: commands

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 const NodeMediaServer = require("node-media-server");
 
 const config = {
-  rtmp: {
-    port: 1935,
-    chunk_size: 60000,
-    gop_cache: true,
-    ping: 30,
-    ping_timeout: 60
-  },
+  // rtmp: {
+  //   port: 1935,
+  //   chunk_size: 60000,
+  //   gop_cache: true,
+  //   ping: 30,
+  //   ping_timeout: 60
+  // },
   http: {
     port: process.env.PORT || 8000,
     allow_origin: "*"
@@ -24,23 +24,23 @@ const config = {
   //     }
   //   ]
   // },
-  relay: {
-    ffmpeg: '/usr/local/bin/ffmpeg',
-    tasks: [
-      {
-        app: 'cctv',
-        mode: 'static',
-        edge: 'rtsp://admin:admin888@192.168.0.149:554/ISAPI/streaming/channels/101',
-        name: '0_149_101',
-        rtsp_transport : 'tcp' //['udp', 'tcp', 'udp_multicast', 'http']
-      },{
-        app: 'mv',
-        mode: 'static',
-        edge: '/Users/visionary/Downloads/videoplayback.mp4',
-        name: 'dq'
-      }
-    ]
-  }
+  // relay: {
+  //   ffmpeg: '/usr/local/bin/ffmpeg',
+  //   tasks: [
+  //     {
+  //       app: 'cctv',
+  //       mode: 'static',
+  //       edge: 'rtsp://admin:admin888@192.168.0.149:554/ISAPI/streaming/channels/101',
+  //       name: '0_149_101',
+  //       rtsp_transport : 'tcp' //['udp', 'tcp', 'udp_multicast', 'http']
+  //     },{
+  //       app: 'mv',
+  //       mode: 'static',
+  //       edge: '/Users/visionary/Downloads/videoplayback.mp4',
+  //       name: 'dq'
+  //     }
+  //   ]
+  // }
 };
 
 var nms = new NodeMediaServer(config);
